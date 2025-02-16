9769edbfa6760c38346bee6e9382c86384dbb5dd
Merin1208 <130655465+Merin1208@users.noreply.github.com>
2025-02-17 04:03:08 +0530

    Delete Department/Department/Department directory

--- Department/Department/Department/src/main/java/com/example/Department/Repository/DepartmentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.Department.Repository;
-
-import com.example.Department.Model.Department;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DepartmentRepository extends JpaRepository<Department, Long> {
-}
